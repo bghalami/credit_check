d7e8cfc15a1be753593a15a732ad9055a3d38142
bghalami <benjaminghalami@gmail.com>
2018-06-27 17:38:23 -0600

    Appropriately named variable

--- credit_check.rb
+++ credit_check.rb
@@ -1,51 +1,40 @@
 card_number = "4024007136512380"
 
 #Seperating the string into elements in an array
-cna = card_number.chars
+card_number_array = card_number.chars
 
 #Converting the strings to integers
-cream = []
-cna.each do |num|
-  cream << num.to_i
+array_to_int = []
+card_number_array.each do |num|
+  array_to_int << num.to_i
 end
 
 #Removing the check number and assigning it to a variable
-check = cream.pop
+check = array_to_int.pop
 
 #Multiplying every other number by 2 starting from right
-feel = []
-cream.each.with_index do |elem, index|
+multiply_array = []
+array_to_int.each.with_index do |elem, index|
   if index % 2 == 0
-    feel << elem * 2
+    multiply_array << elem * 2
   else
-    feel << elem
+    multiply_array << elem
   end
 end
 
-#Old formula
-  # x = -1
-  # while x >= cna.length * (-1)
-  #   if x % 2 != 0
-  #     cna[x] = cna[x] * 2
-  #     x = x - 1
-  #   else
-  #     x = x - 1
-  #   end
-  # end
-
 #Summed digits over 10
-spork = []
-feel.each do |num|
+consolidate = []
+multiply_array.each do |num|
   if num >= 10
-    spork << num - 9
+    consolidate << num - 9
   else
-    spork << num
+    consolidate << num
   end
 end
 
 #Sum of all elements
 sum = 0
-spork.each do |num|
+consolidate.each do |num|
   sum = sum + num
 end
 
