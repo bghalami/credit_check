e4eda4ab58de52d96da7d6e7037578fe82184777
bghalami <benjaminghalami@gmail.com>
2018-06-28 15:20:08 -0600

    trash

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -1,15 +1,15 @@
-require "../credit_check"
+require "../lib/credit_check"
 require "minitest/autorun"
 require "minitest/pride"
 
 class CreditCheckTest < Minitest::Test
   def test_valid_number?
-    credit_check = CreditCheck.new(5541808923795240)
+    credit_check = CreditCheck.new
     assert_equal true, credit_check.valid_number?(5541808923795240)
   end
 
   def test_validation_output
-    credit_check = CreditCheck.new(5541808923795240)
+    credit_check = CreditCheck.new
     assert_equal "The number 5541808923795240 is valid",credit_check.validation_output(5541808923795240)
   end
 end
