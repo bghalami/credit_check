6207348ef1ed08dec16a4cdcf7302337ecf31a43
bghalami <benjaminghalami@gmail.com>
2018-06-28 15:22:49 -0600

    faces to bleed

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -1,15 +1,15 @@
-require "../lib/credit_check"
+require "./credit_check"
 require "minitest/autorun"
 require "minitest/pride"
 
 class CreditCheckTest < Minitest::Test
   def test_valid_number?
-    credit_check = CreditCheck.new
+    credit_check = CreditCheck.new(5541808923795240)
     assert_equal true, credit_check.valid_number?(5541808923795240)
   end
 
   def test_validation_output
-    credit_check = CreditCheck.new
+    credit_check = CreditCheck.new(5541808923795240)
     assert_equal "The number 5541808923795240 is valid",credit_check.validation_output(5541808923795240)
   end
 end
