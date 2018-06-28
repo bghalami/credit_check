11a587f2c847fa81d09b45e6da5a8c1921e5ac95
bghalami <benjaminghalami@gmail.com>
2018-06-28 14:13:29 -0600

    Finished entering methods

--- credit_check.rb
+++ credit_check.rb
@@ -1,56 +1,91 @@
 class CreditCheck
 
   def initialize
-    
+    puts "Enter card number:"
+    card_number = gets
+    @card_number = card_number
+    card_number.to_i
+    @array_as_int = []
+    @check = 0
+    @double_array = []
+    @consolidate = []
+    @sum = 0
+    @card_number_array = []
   end
 
-  def valid_number?(account_number)
-    card_number = account_number.to_s
-    #Seperating the string into elements in an array
-    card_number_array = card_number.chars
-    #Converting the strings to integers
-    array_to_int = []
-    card_number_array.each do |num|
-      array_to_int << num.to_i
+  def seperate_to_array
+    @card_number_array = @card_number.chars
+  end
+
+  def array_to_int
+    @card_number_array.each do |num|
+      @array_as_int << num.to_i
     end
-    #Removing the check number and assigning it to a variable
-    check = array_to_int.pop
-    #Multiplying every other number by 2 starting from right
-    multiply_array = []
-    array_to_int.each.with_index do |elem, index|
+  end
+
+  def remove_check_number
+    @check = @array_as_int.pop
+  end
+
+  def multiply_every_other
+    @array_as_int.each.with_index do |elem, index|
       if index % 2 == 0
-        multiply_array << elem * 2
+        @double_array << elem * 2
       else
-        multiply_array << elem
+        @double_array << elem
       end
-    end
-    #Summed digits over 10
-    consolidate = []
-    multiply_array.each do |num|
-      if num >= 10
-        consolidate << num - 9
-      else
-        consolidate << num
-      end
-    end
-    #Sum of all elements
-    sum = 0
-    consolidate.each do |num|
-      sum = sum + num
-    end
-    #Output
-    if (check + sum) % 10 == 0
-      return true
-    else
-      return false
     end
   end
 
-  def validation_output(account_number)
-    if valid_number?(account_number) == true
-      puts "The number #{account_number} is valid"
+  def consolidate_over_ten
+    @double_array.each do |num|
+      if num >= 10
+        @consolidate << num - 9
+      else
+        @consolidate << num
+      end
+    end
+  end
+
+  def sum
+    @consolidate.each do |num|
+      @sum += num
+    end
+  end
+
+  def activate
+    seperate_to_array
+    array_to_int
+    remove_check_number
+    multiply_every_other
+    consolidate_over_ten
+    sum
+    valid_or_output
+  end
+
+  def valid_or_output
+    puts "To see if this card number is valid, enter [1], to get validation output enter [2]"
+    answer = gets.to_i
+    if answer == 1
+      valid_number?(@card_number)
     else
-      puts "The number #{account_number} is invalid"
+      validation_output(@card_number)
+    end
+  end
+
+  def valid_number?(card_number)
+    if (@check + @sum) % 10 == 0
+      p true
+    else
+      p false
+    end
+  end
+
+  def validation_output(card_number)
+    if valid_number?(@card_number) == true
+      puts "The number #{@card_number} is valid"
+    else
+      puts "The number #{@card_number} is invalid"
     end
   end
 end
